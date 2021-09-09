35ca6d0d1294fa9a34ce978dc6a1381b6f4bc79b
lfenzo <lfenzo@protonmail.com>
2021-09-09 10:51:07 -0300

    Adição do Assign Statement

--- ast/AssignStat.java
+++ ast/AssignStat.java
@@ -1,16 +1,18 @@
-public class AssignStat {
+public class AssignStat extends Stat {
 
     private String id;
-    private Expr expr;
+    private Expr expr; // 'expr' é uma subclasse de Expr
 
     public AssignStat(Var v, Expr expr) {
         this.id = v.getId();
         this.expr = expr;
     }
 
+    @Override
     public void genC() {
-        System.out.printf("\t%s = ", this.id, this.expr.genC());
+        System.out.printf("\t%s = ", this.id);
         this.expr.genC();
+        System.out.printf(";\n");
     }
 
 }
